2d47dd6ceafb747b1a61c777c3ead36095993fc1
KilmanR <avrusin1978@gemail.com>
2026-07-06 19:40:18 +0500

    Initial commit: accounting app with modules/packages

--- dirty_main.py
+++
@@ -1,9 +0,0 @@
-from application.salary import *
-from application.db.people import *
-from datetime import datetime
-from rich import print
-
-if __name__ == '__main__':
-    print(f"[bold cyan]{datetime.now()}[/bold cyan] Запуск [green]dirty_main[/green]")
-    calculate_salary()
-    get_employees()
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
-from rich import print
 
 if __name__ == '__main__':
-    print(f"[bold cyan]{datetime.now()}[/bold cyan] Запуск [green]программы[/green]")
+    print(f"[{datetime.now()}] Запуск программы")
     calculate_salary()
     get_employees()
